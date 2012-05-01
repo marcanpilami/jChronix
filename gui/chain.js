f29345f50f135403834cc5d7b309a11dc2ca139e
marcanpilami <marsu_pilami@msn.com>
2012-05-01 14:54:30 +0200

    true chain states are now drawn on the panel, no just debug data
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- gui/chain.js
+++ gui/chain.js
@@ -6,32 +6,28 @@ var chain;
 
 var paper = null;
 
-function State()
-{
-	
-	
-}
-
-function dragStart(x, y, event)
-{
+function dragStart(x, y, event) {
 	this.xs = this.attr("cx");
 	this.ys = this.attr("cy");
 }
 
-function dragMove(dx, dy, x, y, event)
-{
+function dragMove(dx, dy, x, y, event) {
 	//alert("toto");
 	//this.attr({cx: this.attr("cx") + dx});
-	this.attr({cx: this.xs + dx});
-	this.attr({cy: this.ys + dy});
+	this.attr({
+		cx : this.xs + dx
+	});
+	this.attr({
+		cy : this.ys + dy
+	});
 }
 
-function dragEnd(event)
+function dragEnd(event) {
+
+}
+
+function drawDebug()
 {
-	
-}
-
-function draw() {
 	canvas = document.getElementById("cnMain");
 	ctx = canvas.getContext("2d");
 
@@ -40,62 +36,77 @@ function draw() {
 
 	ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
 	ctx.fillRect(30, 30, 55, 50);
-	
+
 	ctx.strokeText('Hello world!', 0, 50);
+}
 
-
+function drawInit() {
 	paper = new Raphael("raph", 1600, 600);
-	var circle = paper.circle(200,200,50);
-	circle.attr("fill", "#f00");
-	circle.attr({fill: 'firebrick', stroke: 'darkorchid', 'stroke-width': 5});  
-	//circle.animate({"fill": "#f00", "transform": 't100,100'}, 2000, 'linear', function() {});
-	circle.animate({cx:10, cy:10}, 2000, 'linear' );
 	
+	var circle = paper.circle(200, 200, 50);
+	circle.attr("fill", "#f00");
+	circle.attr({
+		fill : 'firebrick',
+		stroke : 'darkorchid',
+		'stroke-width' : 5
+	});
+	//circle.animate({"fill": "#f00", "transform": 't100,100'}, 2000, 'linear', function() {});
+	circle.animate({
+		cx : 10,
+		cy : 10
+	}, 2000, 'linear');
+
 	circle.drag(dragMove, dragStart, dragEnd);
 }
 
-
-function errorCallback(httpStatus, httpStatusText) 
-{
+function errorCallback(httpStatus, httpStatusText) {
 	alert('error ' + httpStatusText);
 }
 
-function successCallback(responseObject) 
-{
+function successCallback(responseObject) {
 	//alert(responseObject.getReturn());
 	$("#greetings").html(responseObject.getReturn() + " - " + uuid.v4());
 }
 
-function getChainOK(responseObject)
-{
-	states.push(responseObject);
+function getChainOK(responseObject) {
 	chain = responseObject.getReturn();
-	
-	/*for ( var i = 0; i < array.length; i++) {
-		
-	}*/
+
+	var ss = chain.getStates().getDTOState();
+
+	for(var i = 0; i < ss.length; i++) {
+		addState(ss[i]);
+	}
 }
 
-function getChainKO(responseObject)
-{
+function addState(DTOState) {
+	states.push(DTOState);
+	var circle = paper.circle(DTOState._x, DTOState._y, 50);
+	circle.attr({
+		fill : 'firebrick',
+		stroke : 'darkorchid',
+		'stroke-width' : 2
+	});
+	circle.drag(dragMove, dragStart, dragEnd);
+}
+
+function getChainKO(responseObject) {
 	alert("oooops");
 }
 
-function loadChain()
-{
+function loadChain() {
 	var p = new internalapi_chronix_oxymores_org__IServiceClientPortType();
 	p.getChain(getChainOK, getChainKO);
 }
 
-function pingServer()
-{
+function pingServer() {
 	var p = new internalapi_chronix_oxymores_org__IServiceClientPortType();
 	p.sayHello(successCallback, errorCallback, "marsu", 12);
 }
 
+
 $(document).ready(function() {
-	draw();
+	drawInit();
 	$("#tabs").tabs();
-//	$("#meuh").addClass("red");  // red is a css class
+	//	$("#meuh").addClass("red");  // red is a css class
 	loadChain();
 });
