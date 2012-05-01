b8733749b1785b07d6c1dc553716285d424dc69a
marcanpilami <marsu_pilami@msn.com>
2012-05-01 15:09:13 +0200

    Dragging states now updates underlying model object
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- gui/chain.js
+++ gui/chain.js
@@ -6,28 +6,27 @@ var chain;
 
 var paper = null;
 
-function dragStart(x, y, event) {
+function dragStateStart(x, y, event) {
 	this.xs = this.attr("cx");
 	this.ys = this.attr("cy");
 }
 
-function dragMove(dx, dy, x, y, event) {
-	//alert("toto");
-	//this.attr({cx: this.attr("cx") + dx});
+function dragStateMove(dx, dy, x, y, event) {
 	this.attr({
 		cx : this.xs + dx
 	});
 	this.attr({
 		cy : this.ys + dy
 	});
+	this.modeldata._x = this.xs + dx;
+	this.modeldata._y = this.ys + dy;
 }
 
-function dragEnd(event) {
+function dragStateEnd(event) {
 
 }
 
-function drawDebug()
-{
+function drawDebug() {
 	canvas = document.getElementById("cnMain");
 	ctx = canvas.getContext("2d");
 
@@ -42,7 +41,8 @@ function drawDebug()
 
 function drawInit() {
 	paper = new Raphael("raph", 1600, 600);
-	
+
+	// Debug animation - don't remove till GUI nearing completion
 	var circle = paper.circle(200, 200, 50);
 	circle.attr("fill", "#f00");
 	circle.attr({
@@ -55,8 +55,6 @@ function drawInit() {
 		cx : 10,
 		cy : 10
 	}, 2000, 'linear');
-
-	circle.drag(dragMove, dragStart, dragEnd);
 }
 
 function errorCallback(httpStatus, httpStatusText) {
@@ -71,22 +69,31 @@ function successCallback(responseObject) {
 function getChainOK(responseObject) {
 	chain = responseObject.getReturn();
 
+	// add chain states to the list and draw them
 	var ss = chain.getStates().getDTOState();
-
 	for(var i = 0; i < ss.length; i++) {
 		addState(ss[i]);
 	}
 }
 
 function addState(DTOState) {
+	// Add state to list
 	states.push(DTOState);
+
+	// Draw state as a circle
 	var circle = paper.circle(DTOState._x, DTOState._y, 50);
 	circle.attr({
 		fill : 'firebrick',
 		stroke : 'darkorchid',
 		'stroke-width' : 2
 	});
-	circle.drag(dragMove, dragStart, dragEnd);
+
+	// Set reference between business model object and representation object
+	DTOState._drawing = circle;
+	circle.modeldata = DTOState;
+
+	// Allow dragging with callback taking connections and business objects into account
+	circle.drag(dragStateMove, dragStateStart, dragStateEnd);
 }
 
 function getChainKO(responseObject) {
