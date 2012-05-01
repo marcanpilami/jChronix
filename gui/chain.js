9b61d50712292006ee6ff5122571b727a7d47c97
marcanpilami <marsu_pilami@msn.com>
2012-05-01 17:30:33 +0200

    Transitions are now drawn and drag correctly
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- gui/chain.js
+++ gui/chain.js
@@ -2,6 +2,7 @@ var canvas;
 var ctx;
 
 var states = new Array();
+var transitions = new Array();
 var chain;
 
 var paper = null;
@@ -12,14 +13,40 @@ function dragStateStart(x, y, event) {
 }
 
 function dragStateMove(dx, dy, x, y, event) {
+	var newx = this.xs + dx;
+	var newy = this.ys + dy;
+
+	// Move state
 	this.attr({
-		cx : this.xs + dx
+		cx : newx
 	});
 	this.attr({
-		cy : this.ys + dy
+		cy : newy
 	});
+
+	// Update state model
 	this.modeldata._x = this.xs + dx;
 	this.modeldata._y = this.ys + dy;
+
+	// Drag transition ends
+	for(var i = 0; i < this.modeldata.trFromHere.length; i++) {
+		var tr = this.modeldata.trFromHere[i];
+		var path = tr.attr("path");
+		path[0][1] = newx;
+		path[0][2] = newy;
+		tr.attr({
+			"path" : path
+		});
+	}
+	for(var i = 0; i < this.modeldata.trReceivedHere.length; i++) {
+		var tr = this.modeldata.trReceivedHere[i];
+		var path = tr.attr("path");
+		path[1][1] = newx;
+		path[1][2] = newy;
+		tr.attr({
+			"path" : path
+		});
+	}
 }
 
 function dragStateEnd(event) {
@@ -74,10 +101,17 @@ function getChainOK(responseObject) {
 	for(var i = 0; i < ss.length; i++) {
 		addState(ss[i]);
 	}
+
+	// add transitions to the list and draw them
+	var trs = chain.getTransitions().getDTOTransition();
+	for(var i = 0; i < trs.length; i++) {
+		addTransition(trs[i]);
+	}
 }
 
 function addState(DTOState) {
-	// Add state to list
+	// Add state to list, both as member of the object (access by id) and to the list itself (enumerator access)
+	states[DTOState._id] = DTOState;
 	states.push(DTOState);
 
 	// Draw state as a circle
@@ -91,9 +125,41 @@ function addState(DTOState) {
 	// Set reference between business model object and representation object
 	DTOState._drawing = circle;
 	circle.modeldata = DTOState;
+	circle.node.id = DTOState._id;
+	circle.id = DTOState._id;
+	DTOState.trFromHere = new Array();
+	DTOState.trReceivedHere = new Array();
 
 	// Allow dragging with callback taking connections and business objects into account
 	circle.drag(dragStateMove, dragStateStart, dragStateEnd);
+}
+
+function addTransition(DTOTransition) {
+	// Add to collection for future reference
+	transitions[DTOTransition._id] = DTOTransition;
+	transitions.push(DTOTransition);
+
+	// Draw as an arrow
+	var from = states[DTOTransition._from];
+	var to = states[DTOTransition._to];
+	var x1 = from._x;
+	var y1 = from._y;
+	var x2 = to._x;
+	var y2 = to._y;
+
+	var arrow = paper.path("M" + x1 + "," + y1 + ",L" + x2 + "," + y2);
+	arrow.attr({
+		"arrow-end" : "classic-wide-long",
+		"stroke-width" : 5
+	});
+
+	// Set cross references to allow easy navigation between SVG and model elements
+	from.trFromHere.push(arrow);
+	to.trReceivedHere.push(arrow);
+	arrow.from = from;
+	arrow.to = to;
+	DTOTransition._drawing = arrow;
+	arrow.modeldata = DTOTransition;
 }
 
 function getChainKO(responseObject) {
