5837f02ef717bc3e4de53446dd813ad1cf6b7284
marcanpilami <marsu_pilami@msn.com>
2012-05-19 12:51:42 +0200

    Added basic log4j logging to web service
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- wapi/src/main/java/org/oxymores/chronix/wapi/ServiceClient.java
+++ wapi/src/main/java/org/oxymores/chronix/wapi/ServiceClient.java
@@ -20,8 +20,10 @@
 
 package org.oxymores.chronix.wapi;
 
+import org.apache.log4j.Logger;
 import org.oxymores.chronix.core.Application;
 import org.oxymores.chronix.core.Chain;
+import org.oxymores.chronix.core.ChronixContext;
 import org.oxymores.chronix.demo.DemoApplication;
 import org.oxymores.chronix.dto.DTOApplication;
 import org.oxymores.chronix.dto.DTOChain;
@@ -30,22 +32,24 @@ import org.oxymores.chronix.internalapi.IServiceClient;
 
 public class ServiceClient implements IServiceClient {
 
+	private static Logger log = Logger.getLogger(ChronixContext.class);
+
 	@Override
 	public String sayHello() {
+		log.debug("Ping service was called");
 		return "houba hop";
 	}
 
 	@Override
 	public DTOApplication getApplication(String name) {
-		System.err.println("oups1");
-		Application a = DemoApplication.getNewDemoApplication(); // TODO: really
-																	// look for
-																	// the
-																	// application
-																	// instead
-																	// of test
-																	// one
-		return Frontier.getApplication(a);
+		log.debug(String.format("getApplication service was called for app %s",
+				name));
+		Application a = DemoApplication.getNewDemoApplication();
+		// TODO: really look for the application instead of test one
+
+		DTOApplication d = Frontier.getApplication(a);
+		log.debug("End of getApplication call. Returning an application.");
+		return d;
 	}
 
 	/*
@@ -56,34 +60,41 @@ public class ServiceClient implements IServiceClient {
 
 	@Override
 	public DTOChain getChain() {
-		System.out.println("oups3");
+		log.debug("getChain service was called");
 		Application a = DemoApplication.getNewDemoApplication();
 		Chain c = a.getChains().get(0);
-		return Frontier.getChain(c);
+		DTOChain d = Frontier.getChain(c);
+		log.debug("End of getChain call. Returning a chain.");
+		return d;
 	}
 
 	@Override
 	public void stageApplication(DTOApplication app) {
 		// TODO Replace test code with true persistence and reboot engine
 		// context
+		log.debug("stageApplication service was called");
 
 		Application a = DemoApplication.getNewDemoApplication();
 		Chain c = a.getChains().get(0);
 
 		System.out.println(app.chains.get(0).states.get(0).getX());
 		System.out.println(c.getStates().get(0).getX());
+
+		log.debug("End of stageApplication call.");
 	}
 
 	@Override
 	public void storeApplication(String uuid) {
 		// TODO Auto-generated method stub
-
+		log.debug("storeApplication service was called");
+		log.debug("End of storeApplication call.");
 	}
 
 	@Override
 	public void resetStage() {
 		// TODO Auto-generated method stub
-
+		log.debug("resetStage service was called");
+		log.debug("End of resetStage call.");
 	}
 
 }
