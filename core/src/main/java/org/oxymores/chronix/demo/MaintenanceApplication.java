1bb7fa867d0528b7530fc3d82f9d9cf7bdd89f30
marcanpilami <marsu_pilami@msn.com>
2012-08-01 21:57:53 +0200

    Added two default applications (chronix maintenance and operations)

---
+++ core/src/main/java/org/oxymores/chronix/demo/MaintenanceApplication.java
@@ -0,0 +1,28 @@
+package org.oxymores.chronix.demo;
+
+import org.oxymores.chronix.core.Application;
+import org.oxymores.chronix.core.Chain;
+import org.oxymores.chronix.core.PlaceGroup;
+import org.oxymores.chronix.core.State;
+
+public class MaintenanceApplication {
+	public static Application getNewApplication() {
+		Application a = PlanBuilder
+				.buildApplication(
+						"Chronix Maintenance",
+						"All the jobs needed to keep the local scheduler node running properly");
+
+		PlaceGroup pg = PlanBuilder.buildDefaultLocalNetwork(a);
+		Chain c1 = PlanBuilder.buildChain(a, "Maintenance plan", "all the default maintenance jobs", pg);
+		
+		State s1 = PlanBuilder.buildNewState(c1, pg, PlanBuilder.buildNewActiveShell(a, "echo purge", "History purge", "Will purge the history table", "-d", "10"));
+		State s2 = PlanBuilder.buildNewState(c1, pg, PlanBuilder.buildNewActiveShell(a, "echo purge", "Trace purge", "Will purge the performance trace table", "-d", "2"));
+		State s3 = PlanBuilder.buildNewState(c1, pg, PlanBuilder.buildNewActiveShell(a, "echo aggre", "Compile purge", "Will aggregate the performance trace table data"));
+		
+		c1.getStartState().connectTo(s1);
+		s1.connectTo(s2);
+		s1.connectTo(s3);
+		
+		return a;
+	}
+}
