cef9f5107d0459002db1adc96fdddf54b0de695a
marcanpilami <marsu_pilami@msn.com>
2012-12-21 08:44:28 +0100

    test xml

---
+++ core/src/test/java/org/oxymores/chronix/core/TestXml.java
@@ -0,0 +1,29 @@
+package org.oxymores.chronix.core;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.oxymores.chronix.engine.TestChain;
+
+public class TestXml {
+	private static Logger log = Logger.getLogger(TestXml.class);
+
+	private String db1;
+	Application a1;
+	
+	
+	@Test
+	public void testXml() throws Exception
+	{
+		Application a1 = org.oxymores.chronix.demo.DemoApplication
+				.getNewDemoApplication();
+
+		JAXBContext ctx = JAXBContext.newInstance(Application.class);
+		Marshaller m = ctx.createMarshaller();
+		m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		
+		m.marshal(a1, System.out);
+	}
+}
