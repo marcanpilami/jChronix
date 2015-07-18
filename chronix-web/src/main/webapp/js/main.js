b5bc64f6d6c16eff417ef4885cec7f8c0b0100d2
marcanpilami <marsu_pilami@msn.com>
2015-07-18 14:11:32 +0200

    better jsplumbing

--- chronix-web/src/main/webapp/js/main.js
+++ chronix-web/src/main/webapp/js/main.js
@@ -1,44 +1,80 @@
 var instance;
 
+function setDN(node, jspInstance)
+{
+    // Not using classic anchors as source, as we want the links to be perimeter links
+    var anch1 = $("<div class='anchor arrow1' style='position: absolute; bottom: -15px; left: 10%;'>remote control</div>");
+    anch1.appendTo(node);
+    var anch2 = $("<div class='anchor arrow2' style='position: absolute; bottom: -15px; left: 70%;'>channel</div>");
+    anch2.appendTo(node);
+
+    jspInstance.makeSource(node, {
+        maxConnections: 100,
+        endpoint: 'Blank',
+        anchor: ["Perimeter", {shape: "Rectangle"}],
+        filter: "div.arrow1",
+        connectorStyle: {strokeStyle: 'red'}
+    });
+
+    jspInstance.makeSource(node, {
+        maxConnections: 100,
+        endpoint: 'Blank',
+        anchor: ["Perimeter", {shape: "Rectangle"}],
+        filter: "div.arrow2",
+        connectorStyle: {strokeStyle: 'blue'}
+    });
+
+    jspInstance.makeTarget(node, {
+        isTarget: true,
+        maxConnections: 100,
+        endpoint: 'Blank',
+        allowLoopback: false,
+        anchor: ["Perimeter", {shape: "Rectangle"}]
+    });
+
+    jspInstance.draggable(node, {containment: "parent", filter: 'div.anchor', filterExclude: true});
+}
+
+function initNetwork()
+{
+    var instance = jsPlumb.getInstance({
+        Connector: ["Bezier", {curviness: 50}],
+        DragOptions: {cursor: "pointer", zIndex: 2000},
+        PaintStyle: {strokeStyle: "gray", lineWidth: 2},
+        EndpointStyle: {radius: 9, fillStyle: "gray"},
+        ConnectionOverlays: [['Arrow', {width: 12, location: 1}]],
+        HoverPaintStyle: {strokeStyle: "#ec9f2e"},
+        EndpointHoverStyle: {fillStyle: "#ec9f2e"},
+        Container: "node-c"
+    });
+
+    $('.dn').each(function ()
+    {
+        setDN(this, instance);
+    });
+
+    $('#node-c').droppable({drop: function (event, ui) {
+            var t = ui.helper.clone();
+            t.appendTo(this);
+            setDN(t, instance);
+        }});
+    $('.paletteitem').draggable({helper: 'clone', appendTo: '#node-c'});
+
+    $('.dn').click(function ()
+    {
+        $('.dn').removeClass('drawingPanel-selected');
+        $(this).addClass('drawingPanel-selected');
+    });
+}
+
 $(
         function ()
         {
-            $('#tabs').tabs(
-                    {
-                        active: 0,
-                        heightStyle: 'fill',
-                    });
-            
-            
-            
-            instance = jsPlumb.getInstance({
-                Connector: ["Bezier", {curviness: 50}],
-                DragOptions: {cursor: "pointer", zIndex: 2000},
-                PaintStyle: {strokeStyle: "gray", lineWidth: 2},
-                EndpointStyle: {radius: 9, fillStyle: "gray"},
-                HoverPaintStyle: {strokeStyle: "#ec9f2e"},
-                EndpointHoverStyle: {fillStyle: "#ec9f2e"},
-                Container: "node-c"
+            $('#tabs').tabs({
+                active: 0,
+                heightStyle: 'fill'
             });
-
-            $('.execnode').each(function ()
-            {
-                instance.addEndpoint(this, {
-                    uuid: this.id + "-bottom",
-                    anchor: "Bottom",
-                    maxConnections: -1,
-                    isSource: true,
-                });
-                
-                instance.addEndpoint(this, {
-                    uuid: this.id + "-top",
-                    anchor: "Top",
-                    maxConnections: -1,
-                    isTarget: true,
-                });
-            });
-
-            instance.draggable($('.execnode'));
-            //instance.repaintEverything();
+            
+            initNetwork();
         }
 );
