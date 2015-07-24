e0918a17b26ab15340c16c34e6f7350a51b74e53
marcanpilami <marsu_pilami@msn.com>
2015-07-24 16:51:54 +0200

    GUI: added group view panel

--- chronix-web/src/main/webapp/js/app.js
+++ chronix-web/src/main/webapp/js/app.js
@@ -5,14 +5,14 @@ function initApp(uuid)
     var chainPanel = null;
     var seqPanel = null;
     var clockPanel = null;
-    
+
     $.getJSON("ws/meta/app/id/" + uuid).done(function (data)
     {
         app = data;
 
         content.load("app.html", function ()
         {
-            // Replace ids  
+            // Replace ids
             content.html(content[0].innerHTML.replace(/IDIDID/g, uuid));
 
             // Setup
@@ -40,6 +40,10 @@ function initApp(uuid)
                     {
                         clockPanel.initPanel();
                     }
+                    if (i.indexOf("app-group") === 0)
+                    {
+                        initGroup(app);
+                    }
                 }
             });
 
@@ -47,7 +51,7 @@ function initApp(uuid)
             chainPanel = new PanelChain(app);
             seqPanel = new PanelRec(app);
             clockPanel = new PanelClock(app);
-            
+
             // Open first tab
             initCommand(app);
         });
@@ -58,7 +62,7 @@ function initCommand(app)
 {
     var content = $("#app-command-" + app.id);
     var commands = app.shells;
-    
+
     content.empty();
     new Handsontable(content[0], {
         data: commands,
@@ -82,7 +86,7 @@ function initExternal(app)
 {
     var content = $("#app-external-" + app.id);
     var externals = app.externals;
-    
+
     content.empty();
     new Handsontable(content[0], {
         data: externals,
@@ -97,9 +101,46 @@ function initExternal(app)
             {data: 'name', title: 'Name'},
             {data: 'description', title: 'Description'},
             {data: 'accountRestriction', title: 'Optional shared secret'},
-            {data: 'regularExpression', title:'Calendar occurrence extractor (regular expression)'}
+            {data: 'regularExpression', title: 'Calendar occurrence extractor (regular expression)'}
         ],
         afterChange: initIdIfNone
     });
-    
+}
+
+function initGroup(app)
+{
+    var content = $("#app-group-" + app.id);
+    var groups = app.groups;
+
+    content.empty();
+    new Handsontable(content[0], {
+        data: groups,
+        minSpareRows: 1,
+        rowHeaders: true,
+        colHeaders: true,
+        contextMenu: ['remove_row', 'undo', 'redo'],
+        manualColumnResize: true,
+        manualRowResize: false,
+        columns: [
+            {data: 'name', title: 'Name'},
+            {data: 'description', title: 'Description'}
+        ],
+        dataSchema: function () {
+            var res = new Object();
+            res.id = uuid.v4();
+            return res;
+        },
+        beforeRemoveRow: function (row)
+        {
+            var c = this.getSourceDataAtRow(row);
+            $.each(network.places, function ()
+            {
+                var place = this;
+                $.each(place.memberOf, function ()
+                {
+                    place.memberOf.splice(place.memberOf.indexOf(c.id), 1);
+                });
+            });
+        }
+    });
 }
